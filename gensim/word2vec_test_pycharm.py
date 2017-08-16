ce0a17ace9e87ad6e03d32de635fc577916ab7f8
Cherrue <th885172@gmail.com>
2017-08-17 01:31:30 +0900

    word2vec training code

--- gensim/word2vec_test_pycharm.py
+++ gensim/word2vec_test_pycharm.py
@@ -1,3 +1,4 @@
+#train
 # import gensim, logging
 # import codecs
 # logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -27,27 +28,32 @@ from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import pandas as pd
 model = gensim.models.Word2Vec.load('word2vec_test_pychar_model')
+# scatter
 #print(new_model.wv['선보'])
+#
+# vocab = list(model.wv.vocab)
+# X = model[vocab]
+# sample_X = X[:100]
+#
+# tsne = TSNE(n_components=2)
+# X_tsne = tsne.fit_transform(sample_X)
+# print("df start")
+# df = pd.concat([pd.DataFrame(X_tsne),
+#                 pd.Series(vocab)],
+#                axis=1)
+#
+# print("df finish")
+# df.columns = ['x', 'y', 'word']
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(1, 1, 1)
+#
+# ax.scatter(df['x'], df['y'])
+#
+# #for i, txt in enumerate(df['word']):
+# #    ax.annotate(txt, (df['x'].iloc[i], df['y'].iloc[i]))
+#
+# fig.show()
 
-vocab = list(model.wv.vocab)
-X = model[vocab]
-
-tsne = TSNE(n_components=2)
-X_tsne = tsne.fit_transform(X)
-print("df start")
-df = pd.concat([pd.DataFrame(X_tsne),
-                pd.Series(vocab)],
-               axis=1)
-
-print("df finish")
-df.columns = ['x', 'y', 'word']
-
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-
-ax.scatter(df['x'], df['y'])
-
-#for i, txt in enumerate(df['word']):
-#    ax.annotate(txt, (df['x'].iloc[i], df['y'].iloc[i]))
-
-fig.show()
+# test
+print(model.most_similar(positive=['보영','정석']))
